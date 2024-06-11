8f269fabd6a4f761a92888b088aa371f3ca48b14
LHCRTassell <lhcrtassell@gmail.com>
2024-06-11 09:22:29 -0300

    Update TRABALHINHO DE IVNA.py

--- TRABALHINHO DE IVNA.py
+++ TRABALHINHO DE IVNA.py
@@ -2,36 +2,39 @@ import Rpi.GPIO as GPIO
 import time
 contador=0
 GPIO.setmode(GPIO.BOARD)
+11 == variavel1
+12 == variavel2
+13 == variavel3
 
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
+GPIO.setup(variavel1,GPIO.OUT)
+GPIO.setup(variavel2,GPIO.OUT) 
+GPIO.setup(variavel3,GPIO.OUT) 
 
 GPIO.setup(18,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-GPIO.output(11,1)
-GPIO.output(12,1)
-GPIO.output(13,1)
+GPIO.output(variavel1,1) 
+GPIO.output(variavel2,1) 
+GPIO.output(variavel3,1) 
 try:
 	while(1):
 		if GPIO.input(18)==GPIO.LOW:
 			contador=contador+1
 			time.sleep(0.5)
 		if contador==1:
-			GPIO.output(11,0)
-			GPIO.output(12.1)
-			GPIO.output(13,1)
+			GPIO.output(variavel1,0)
+			GPIO.output(variavel2,1)
+			GPIO.output(variavel3,1)
 		elif contador==2:
-			GPIO.output(11,1)
-			GPIO.output(12.0)
-			GPIO.output(13,1)
+			GPIO.output(variavel1,1)
+			GPIO.output(variavel2,0)
+			GPIO.output(variavel3,1)
 		elif contador==3:
-			GPIO.output(11,1)
-			GPIO.output(12,1)
-			GPIO.output(13,0)
+			GPIO.output(variavel1,1)
+			GPIO.output(variavel2,1)
+			GPIO.output(variavel3,0)
 		elif contador>=4:
-			GPIO.output(11,1)
-			GPIO.output(12,1)
-			GPIO.output(13,1)
+			GPIO.output(variavel1,1)
+			GPIO.output(variavel2,1)
+			GPIO.output(variavel3,1)
 		if contador==4:
 			contador=0
 		time.sleep(0.1)
